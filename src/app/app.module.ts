4787c3b9382f66c2ee4ebb99151772c6cfd23a20
rayssa neder <rayssa.neder@techne.com.br>
2021-04-21 14:35:36 -0300

    Adiciona componente nova trensferencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NovaTranferenciaComponent } from './nova-tranferencia/nova-tranferencia/nova-tranferencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTranferenciaComponent
   ],
   imports: [
     BrowserModule,
